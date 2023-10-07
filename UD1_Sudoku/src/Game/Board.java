a62d6f5ede36f55249603158cb76d0c85a45d11f
Xoubas <a21javierbq@iessanclemente.net>
2023-10-07 20:26:34 +0200

    GenerateBoard

--- UD1_Sudoku/src/Game/Board.java
+++ UD1_Sudoku/src/Game/Board.java
@@ -1,19 +1,57 @@
 package Game;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
 
 public class Board {
 	private final char[][][] grid;
 
 	public Board() {
-		grid = new char[3][3][3];
+		grid = new char[9][3][3];
 	}
 
 	public Board(int n) {
-		grid = new char[n][n][n];
+		int boxes = n * n;
+		grid = new char[boxes][n][n];
 	}
 
 	public void setGrid(HashSet<Character> hs) {
-		
+		for (int i = 0; i < grid.length; i++) {
+			grid[i] = fillBox(hs);
+			checkBox(grid[i]);
+		}
+	}
+
+	private char[][] fillBox(HashSet<Character> hs) {
+		char[][] box = null;
+		Iterator<Character> ite = hs.iterator();
+		for (int j = 0; j < grid[0].length; j++) {
+			for (int k = 0; k < grid[0][0].length; k++) {
+				Random ran = new Random();
+				int n = ran.nextInt(hs.size());
+				while (n >= 0)
+					box[j][k] = ite.next();
+			}
+		}
+		return box;
+	}
+
+	private void checkBox(char[][] box) {
+		for (int i = 0; i < grid[0].length; i++) {
+			if (grid[i].equals(box)) {
+
+			}
+		}
+
+	}
+
+	public void shuffle() {
+
+	}
+
+	public void shuffleSymbols() {
+
 	}
 }
