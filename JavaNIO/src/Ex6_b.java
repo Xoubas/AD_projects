6a26dbdf350797eef7fa98ad1b078d0da1179023
Xoubas <a21javierbq@iessanclemente.net>
2023-10-19 23:12:00 +0200

    NIOExercises

---
+++ JavaNIO/src/Ex6_b.java
@@ -0,0 +1,20 @@
+/*
+Emplea un JFileChooser para seleccionar la carpeta a explorar. Además, debe
+realizar la comprobación de la existencia y de que es un directorio.
+Realiza un programa que muestre el listado del contenido de un directorio de
+los siguientes modos:
+a) Repite el ejercicio anteriormente realizado que muestre el contenido de todos los
+archivos y directorios de una carpeta dada con el método listFiles() de File, pero a
+partir de un Path.
+El método listFiles() devuelve una array de objetos File que son el contenido
+del directorio:
+b) De manera recursiva muestre todo el contenido, incluidos subdirectorios:
+Crea un ArrayList para guardar las rutas.
+Crea un método recorrer, que, de manera recursiva recoja el archivo o
+directorio:
+ */
+public class Ex6_b {
+    public static void main(String[] args) {
+
+    }
+}
