f26eddad2975c08aa926e31096ff0b943e77c673
Javi <a21javierbq@iessanclemente.net>
2023-11-20 10:37:22 +0100

    Biblioteca

---
+++ Biblioteca_BD/src/main/java/org/example/Book.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Book {
+    private int idBook;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int anho;
+    private boolean disponible;
+    private byte[] portada;
+}
---
+++ Biblioteca_BD/src/main/java/org/example/LibraryConnectionManager.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class LibraryConnectionManager {
+    private static volatile LibraryConnectionManager connection;
+
+    // Código Java para crear una clase singleton mediante
+// "Inicialización Ansiosa"
+//    public class ClaseSingletonAD {
+//        // instancia pública inicializada al cargar la clase
+//        private static final ClaseSingletonAD instance = new ClaseSingletonAD();
+//
+//        private ClaseSingletonAD() {
+//            // constructor privado
+//        }
+//
+//        public static ClaseSingletonAD getInstance(){
+//            return instance;
+//        }
+//    }
+}
